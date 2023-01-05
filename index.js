fec97982a44b5301e044b7295923ee7275432647
nouman1105 <nouman1105@gmail.com>
2023-01-05 20:17:51 +0700

    index.js starting

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const { createCanvas, loadImage } = require('canvas');
 const canvas = createCanvas(1000, 1000);
 const ctx = canvas.getContext("2d");
 
+const saveLayer = (_canvas) => {
+    fs.writeFileSync('./newImage.png', _canvas.toBuffer('image/png'));
+}
+
 const drawLayer = async () => {
-    const image = await loadImage('eye_ball.png');
+    const image = await loadImage('./eye_ball.png');
     ctx.drawImage(image, 0, 0, 1000, 1000);
+    console.log("This ran..."); 
+    saveLayer(canvas);
 }
 
 drawLayer();
